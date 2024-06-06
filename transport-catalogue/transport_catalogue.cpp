1be9ddbe22f904e880227b8eb80797a7492e4c4d
GreeAI <danilaf27@mail.ru>
2024-06-06 22:50:04 +0400

    Первый раз... Жду второго

--- transport-catalogue/transport_catalogue.cpp
+++ transport-catalogue/transport_catalogue.cpp
@@ -1,1 +1,69 @@
-// место для вашего кода
+#include "transport_catalogue.h"
+#include "stat_reader.h"
+
+void TransportCatalogue::AddStop(const std::string& name, const detail::Coordinates& coords) {
+    stops_.emplace_back(name, coords);
+    const Stop& stop = stops_.back();
+    buses_to_stop_[&stop] = {};
+    stopname_.emplace(stop.name, stop);
+
+}
+
+void TransportCatalogue::AddBus(const std::string& name, const std::vector<std::string_view>& stops) {
+    TransportCatalogue::Bus bus_to_add;
+    bus_to_add.name = name;
+    for (const std::string_view& stop : stops) {
+        auto it = stopname_.find(stop);
+        if (it != stopname_.end()) {
+            const Stop& stop_ref = it->second;
+            bus_to_add.stops.emplace_back(&stop_ref);
+        }
+    }
+    buses_.emplace_back(std::move(bus_to_add));
+    const TransportCatalogue::Bus& bus = buses_.back();
+    for (const TransportCatalogue::Stop* path : bus.stops) {
+        buses_to_stop_[path].emplace(static_cast<std::string_view>(bus.name));
+    }
+    const TransportCatalogue::Bus& bus_ref = buses_.back();
+    busname_.emplace(bus_ref.name, bus_ref);
+
+}
+
+const TransportCatalogue::Stop* TransportCatalogue::FindStop(std::string_view name) const {
+    auto it = stopname_.find(name);
+    return it != stopname_.end() ? &it->second : nullptr;
+}
+
+const TransportCatalogue::Bus* TransportCatalogue::FindBus(std::string_view name) const {
+    auto it = busname_.find(name);
+    return it != busname_.end() ? &it->second : nullptr;
+}
+
+double TransportCatalogue::GetDistance(const TransportCatalogue::Bus& bus) const {
+    double distance = 0.0;
+    if (bus.stops.size() > 1) {
+        for (size_t i = 0; i < bus.stops.size() - 1; ++i) {
+            distance += ComputeDistance(bus.stops[i]->coordinates, bus.stops[i + 1]->coordinates);
+        }
+    }
+    return distance;
+}
+
+std::set<std::string_view> TransportCatalogue::GetUniqueBus(const Stop* name) const {
+    std::set<std::string_view> buss;
+    if (buses_to_stop_.count(name) == 1) {
+        for (const auto& [stop_name, name_bus] : buses_to_stop_) {
+            if (stop_name == name) {
+                for (const auto& bus : name_bus) {
+                    buss.insert(bus);
+                }
+            }
+        }
+    }
+
+    return buss;
+}
+
+std::unordered_set<const TransportCatalogue::Stop*> TransportCatalogue::GetUniqueStops(const TransportCatalogue::Bus& bus) const {
+    return std::unordered_set<const TransportCatalogue::Stop*>(bus.stops.begin(), bus.stops.end());
+}
--- transport-catalogue/transport_catalogue.h
+++ transport-catalogue/transport_catalogue.h
@@ -1,1 +1,41 @@
-// место для вашего кода
+#pragma once
+
+#include <deque>
+#include <string>
+#include <string_view>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+#include <set>
+
+#include "geo.h"
+
+
+class TransportCatalogue {
+public:
+    struct Bus;
+
+    struct Stop {
+        std::string name;
+        detail::Coordinates coordinates;
+    };
+
+    struct Bus {
+        std::string name;
+        std::vector<const Stop*> stops;
+    };
+    void AddStop(const std::string& name, const detail::Coordinates& coords);
+    void AddBus(const std::string& bus, const std::vector<std::string_view>& stops);
+    const Stop* FindStop(std::string_view stop_name) const;
+    const Bus* FindBus(std::string_view bus_name) const;
+    double GetDistance(const Bus& bus) const;
+    std::set<std::string_view> GetUniqueBus(const Stop* name) const;
+    std::unordered_set<const Stop*> GetUniqueStops(const Bus& bus) const;
+
+private:
+    std::deque<Stop> stops_;
+    std::deque<Bus> buses_;
+    std::unordered_map<std::string_view, const Stop&> stopname_;
+    std::unordered_map<std::string_view, const Bus&> busname_;
+    std::unordered_map<const Stop*, std::set<std::string_view>> buses_to_stop_;
+};
